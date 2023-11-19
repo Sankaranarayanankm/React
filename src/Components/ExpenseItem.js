ca4037752c39381ab413efff83d685e334a57574
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2023-11-19 10:32:53 +0530

    added random expense items

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ExpenseItem(){
+  return (
+    <div>
+      <h1>Expense Items</h1>
+      <ul>
+        <li>Food : 10rs</li>
+        <li>Petrol : 100rs</li>
+        <li>Movies : 200rs</li>
+      </ul>
+    </div>
+  )
+}
+export default ExpenseItem; 
